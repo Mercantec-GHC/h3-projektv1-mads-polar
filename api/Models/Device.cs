82d8b1b9860f77dc5de5e2403a1d2100c3bbcbfb
Patrick <patricktl2004@gmail.com>
2024-10-17 18:56:55 +0200

    Created Device model

---
+++ api/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Models
+{
+    public class Device : Common
+    {
+        public string Status { get; set; } //Armed, disarmed or detecting motion
+        public string DeviceId { get; set; }
+    }
+
+    public class DeviceDTO
+    {
+        public string DeviceId { get; set }
+        public string Status { get; set; }
+    }
+    }
+}
